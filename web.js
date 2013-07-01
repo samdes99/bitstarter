f76af37e3ffbbceea6fc5de72747b3b263398fa4
Sam <samdes99@yahoo.com>
2013-07-01 07:18:02 -0400

    Updated web.js again

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
-var express = require('express');
 
+var express = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+ //  response.send('Hello World2!');
+        var buff = null;
+	fs.readFile('index.html', function (err, data) {
+             if (err) throw err;
+             buff = new Buffer(data);
+            
+     });
+      response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
